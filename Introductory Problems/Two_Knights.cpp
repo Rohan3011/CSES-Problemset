9b2b9c66d76c40f8f9152833af247c729301a069
Rohanop <rohankamble3011@gmail.com>
2022-01-21 20:54:00 +0530

    changed to ll

--- Introductory Problems/Two_Knights.cpp
+++ Introductory Problems/Two_Knights.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 using namespace std;
 
-#define int long long
-
-int two_knights(int n) {
+long long two_knights(long long n) {
   // choose 2 out of n*n cells
-  int total = n * n * (n * n - 1) / 2;
+  long long total = n * n * (n * n - 1) / 2;
 
   // squares : 2*3 and 3*2
-  int attacking = 2 * 2 * (n - 1) * (n - 2);
+  long long attacking = 2 * 2 * (n - 1) * (n - 2);
   return total - attacking;
 }
 
 signed main() {
-  int n;
+  long long n;
   cin >> n;
-  for (int i = 1; i <= n; i++)
+  for (long long i = 1; i <= n; i++)
     cout << two_knights(i) << "\n";
 }
